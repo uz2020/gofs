a286f176e6b09b709112d27ffbce768d8020583b
uz2020 <rongzhj2020@163.com>
2021-05-08 23:02:58 +0800

    1

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/urfave/cli v1.22.5
+	golang.org/x/tools v0.1.0 // indirect
 	unknwon.dev/clog/v2 v2.2.0
 )
--- internal/cmd/controllers/file.go
+++ internal/cmd/controllers/file.go
@@ -2,12 +2,14 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func UploadFile(c *gin.Context) {
 }
 
 func AcquireFileName(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"file_name": 5})
 }
 
 func GetFile(c *gin.Context) {
